531a8dffd9c9e6f835fc33d27a49ac032d8059df
Fardeen Ahamed <1si20et403@sit.ac.in>
2024-03-16 07:32:36 +0530

    todo-full-stack-backend

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import DbConnection from "./database/db.js";
+import cors from "cors";
+import route from "./routes/route.js";
+import authRoute from "./routes/authRoute.js";
+const app = express();
+const PORT = 8000;
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", route);
+app.use("/", authRoute);
+
+DbConnection();
+app.listen(PORT, () => {
+  console.log(`your port is successfully running on port:${PORT}`);
+});
